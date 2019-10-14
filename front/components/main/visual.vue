7f4e72806c61d2791f25ca92b811a859023711dc
LOTTEWORLD\dev2057 <kineage18@naver.com>
2019-10-14 09:06:02 +0900

    no message

--- front/components/main/visual.vue
+++ front/components/main/visual.vue
@@ -2,11 +2,21 @@
     <div class="main-visual">
         <div class="main-visual__list">
             <ul>
-                <li><router-link to="/" style="background-image:url( /images/common/banner_sample.jpg );"></router-link></li>
-                <li><router-link to="/" style="background-image:url( /images/common/banner_sample.jpg );"></router-link></li>
-                <li><router-link to="/" style="background-image:url( /images/common/banner_sample.jpg );"></router-link></li>
-                <li><router-link to="/" style="background-image:url( /images/common/banner_sample.jpg );"></router-link></li>
-                <li><router-link to="/" style="background-image:url( /images/common/banner_sample.jpg );"></router-link></li>
+                <li>
+                    <router-link to="/"></router-link>
+                </li>
+                <li>
+                    <router-link to="/"></router-link>
+                </li>
+                <li>
+                    <router-link to="/"></router-link>
+                </li>
+                <li>
+                    <router-link to="/"></router-link>
+                </li>
+                <li>
+                    <router-link to="/"></router-link>
+                </li>
             </ul>
         </div>
     </div>
@@ -33,12 +43,9 @@ export default {
     .main-visual{ margin-bottom: 10px;
     .main-visual__list{
         ul{ position: relative; width: 100%; height: 350px;
-            li{ position: absolute; width: 100%;
-                a{ display: block; 
-                    background-image : url( /images/common/banner_sample.jpg );
-                    // background-image : url( ~@/assets/banner_sample.jpg );
-                    background-image: none; background-size: cover; background-position: center; height: 350px;
-                }
+            li{ position: absolute; width: 100%; 
+                a{ display: block; width: 100%; height: 350px; background-image: url( ~static/images/common/banner_sample.jpg ); background-size: cover; background-position: center; }
+                
             }
         }
     }

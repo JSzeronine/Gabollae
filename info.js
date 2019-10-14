fb44b398f8fa656f09c4b9a88c1487642e9c0a8b
LOTTEWORLD\dev2057 <kineage18@naver.com>
2019-10-14 17:32:24 +0900

    swiper setting

--- info.js
+++ info.js
@@ -17,3 +17,68 @@
 
 
 
+// npm install vue-awesome-swiper --save
+
+// module.exports = {
+//     plugins : [
+//         { src : "~/plugins/swiper.js", ssr : false }
+//     ],
+
+//     modules : [
+//         "@nuxtjs/style-resources",
+//     ],
+
+//     styleResources : {
+//         scss : [
+//             "~/assets/scss/_function.scss"
+//         ],
+//     },
+
+//     css : [
+//         "swiper/dist/css/swiper.css"
+//     ]
+// }
+
+// default.vue
+// import Vue from 'vue';
+
+// if (process.browser) {
+//     const VueAwesomeSwiper = require('vue-awesome-swiper/dist/ssr')
+//     Vue.use(VueAwesomeSwiper)
+// }
+
+
+
+
+
+// Headers.vue
+/* <template>
+    <div class="header">
+        <div v-swiper:mySwiper="swiperOption">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide" v-for="( item, index ) in items" :key="index">
+                    <router-link :to="item">
+                        {{ item }}
+                    </router-link>
+                </div>
+            </div>
+
+            <div class="swiper-pagination"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            items : [ "/", "/profile", "/signup", ],
+            swiperOption : {
+                pagination : {
+                    el : ".swiper-pagination"
+                }
+            }
+        }
+    }
+}
+</script> */

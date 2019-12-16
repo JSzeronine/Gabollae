16fddbc5eaa477159ce99e048b63511e238a7577
jszeronine <kineage18@naver.com>
2019-12-16 15:35:34 +0900

    서버 주소 변경

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -62,8 +62,8 @@ module.exports = {
     axios : {
         // browserBaseURL : "http://10.105.157.58:3085",
         // baseURL : "http://10.105.157.58:3085",
-        browserBaseURL : process.env.NODE_ENV === "production" ? "https://api.gagoboja.com" : "http://localhost:3085",
-        baseURL : process.env.NODE_ENV === "production" ? "https://api.gagoboja.com" : "http://localhost:3085",
+        browserBaseURL : process.env.NODE_ENV === "production" ? "https://3.133.211.47" : "http://localhost:3085",
+        baseURL : process.env.NODE_ENV === "production" ? "https://3.133.211.47" : "http://localhost:3085",
         https : false,
     },
     

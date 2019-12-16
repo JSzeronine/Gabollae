4cfa6f95c902e6a1d7cea28aa540eba073a40aac
jszeronine <kineage18@naver.com>
2019-12-16 20:48:58 +0900

    global 설정

--- front/plugins/global.js
+++ front/plugins/global.js
@@ -1,6 +1,7 @@
 export default{
     install( Vue ){
         // Vue.prototype.getResourceURL = "http://10.105.157.58:3085/";
-        Vue.prototype.getResourceURL = "http://localhost:3085/";
+        // Vue.prototype.getResourceURL = "http://localhost:3085/";
+        Vue.prototype.getResourceURL = "";
     }
 }
